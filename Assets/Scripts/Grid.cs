e5393901fd08bcc30f55f2eda319f6a0855ed910
jorgevilasboas <jorge.sergipe@gmail.com>
2017-06-12 09:34:29 -0300

    first commit

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,172 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Grid : MonoBehaviour
+{
+    
+
+    public Transform hexPrefab;
+
+   
+    public int gridWidth = 11;
+    public int gridHeight = 11;
+
+    float hexWidth = 1.082f;
+    float hexHeight = 1.24f;
+    public float gap = 0.0f;
+
+    public Sprite home;
+
+    public Sprite collected;
+
+    ArrayList coletados;
+    ArrayList casa;
+
+    Vector3 startPos;
+
+    Cell[,] malha;
+    System.Collections.Generic.Dictionary<GameObject, Vector2> map;
+
+    void Start()
+    {
+        malha = new Cell[gridHeight,gridWidth];
+        map = new System.Collections.Generic.Dictionary<GameObject, Vector2>();
+        coletados = new ArrayList();
+        casa = new ArrayList();
+        AddGap();
+        CalcStartPos();
+        CreateGrid();
+    }
+
+    void AddGap()
+    {
+        hexWidth += hexWidth * gap;
+        hexHeight += hexHeight * gap;
+
+        
+    }
+
+    void CalcStartPos()
+    {
+        float offset = 0;
+        if (gridHeight / 2 % 2 != 0)
+            offset = hexWidth / 2;
+
+        float x = -hexWidth * (gridWidth / 2) - offset;
+        float y = hexHeight * 0.75f * (gridHeight / 2);
+
+        startPos = new Vector3(x, y, 0);
+    }
+
+    Vector3 CalcWorldPos(Vector2 gridPos)
+    {
+        float offset = 0;
+        if (gridPos.y % 2 != 0)
+            offset = hexWidth / 2;
+
+        float x = startPos.x + gridPos.x * hexWidth + offset;
+        float y = startPos.y - gridPos.y * hexHeight * 0.75f;
+
+        return new Vector3(x, y, 0);
+    }
+
+    public void trataColisao(GameObject hex)
+    {
+        SpriteRenderer sr = hex.GetComponent<SpriteRenderer>();
+        sr.sprite = collected;
+        if ((!coletados.Contains(hex))&&(!casa.Contains(hex)))
+            coletados.Add(hex);
+        else if (casa.Contains(hex)&&coletados.Count>0 )
+        {
+            fazcasa();
+        }
+        Debug.Log(hex.name);
+    }
+
+    private void fazcasa()
+    {
+        
+    }
+
+    void CreateGrid()
+    {
+        for (int y = 0; y < gridHeight; y++)
+        {
+            for (int x = 0; x < gridWidth; x++)
+            {
+                Transform hex = Instantiate(hexPrefab) as Transform;
+                Vector2 gridPos = new Vector2(x, y);
+                hex.position = CalcWorldPos(gridPos);
+                hex.parent = this.transform;
+                hex.name = "Hexagon" + x + "|" + y;
+
+                //Fora do original:
+                GameObject hexgameob = hex.gameObject;
+                malha[x,y] = new Cell(x,y,hexgameob);
+                map.Add(hexgameob,new Vector2(x,y));
+                if (x==5 && y == 5){
+                       SpriteRenderer sr = hexgameob.GetComponent<SpriteRenderer>();
+                    sr.sprite = home;
+                }
+            }
+        }
+    }
+
+    public class Cell
+    {
+        GameObject hex;
+
+        int x;
+        int y;
+
+        public Cell(int x, int y, GameObject hex)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public GameObject Hex
+        {
+            get
+            {
+                return hex;
+            }
+
+            set
+            {
+                hex = value;
+            }
+        }
+
+        int X
+        {
+            get
+            {
+                return x;
+            }
+
+            set
+            {
+                x = value;
+            }
+        }
+
+        int Y
+        {
+            get
+            {
+                return y;
+            }
+
+            set
+            {
+                y = value;
+            }
+        }
+    }
+}
+
+
+
+
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	
+
+
+
+
+    public Rigidbody2D rb;
+    public bool Analogico = true;
+    public float Velocity = 5;
+    public GameObject gm;
+    Grid grid;
+
+    
+    // Use this for initialization
+    void Start()
+    {
+      
+        grid = (Grid)gm.GetComponent(typeof(Grid));
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+     //   Debug.Log(coletados.Count);
+
+
+
+        float v; float h;
+
+
+        bool moverCima = Input.GetKey(KeyCode.W);
+        bool moverBaixo = Input.GetKey(KeyCode.S);
+        bool moverDireita = Input.GetKey(KeyCode.D);
+        bool moverEsquerda = Input.GetKey(KeyCode.A);
+
+        //Analogico
+
+        if (Analogico)
+        {
+
+            v = Input.GetAxis("Vertical");
+            h = Input.GetAxis("Horizontal");
+        }
+
+        //Digital
+        else
+        {
+
+            if (moverCima)
+            {
+                v = 1;
+            }
+            else if (moverBaixo)
+            {
+                v = -1;
+            }
+            else
+            {
+                v = 0;
+            }
+
+            if (moverDireita)
+            {
+                h = 1;
+            }
+            else if (moverEsquerda)
+            {
+                h = -1;
+            }
+            else
+            {
+                h = 0;
+            }
+        }
+        this.rb.velocity = new Vector2(h * Velocity, v * Velocity);
+
+
+
+        //Identifica SpaceBar
+
+     
+    
+
+
+
+
+
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+       
+        grid.trataColisao(collider.gameObject);
+       
+        
+
+    }
+}
