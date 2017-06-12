5f1a0086783617d670a64a65540b8226b7f4a153
jorgevilasboas <jorge.sergipe@gmail.com>
2017-06-12 15:36:10 -0300

    Adding player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
-
-	
-
-
-
+public class Player : MonoBehaviour {    
 
     public Rigidbody2D rb;
-    public bool Analogico = true;
-    public float Velocity = 5;
     public GameObject gm;
     Grid grid;
+
+    public float speed = 3.5f;
+    public float currentRotation;
+    public float rotationSensitivity = 50.0f;
 
     
     // Use this for initialization
@@ -23,75 +20,28 @@ public class Player : MonoBehaviour {
      
     }
 
+    void FixedUpdate()
+    {
+        MoveForward();
+        Rotate();
+    }
+
+    void MoveForward()
+    {
+        transform.position += transform.up * speed * Time.deltaTime;
+    }
+
+    void Rotate()
+    {
+        transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, transform.rotation.y, currentRotation));
+    }
+
     // Update is called once per frame
     void Update()
     {
-
-     //   Debug.Log(coletados.Count);
-
-
-
-        float v; float h;
-
-
-        bool moverCima = Input.GetKey(KeyCode.W);
-        bool moverBaixo = Input.GetKey(KeyCode.S);
-        bool moverDireita = Input.GetKey(KeyCode.D);
-        bool moverEsquerda = Input.GetKey(KeyCode.A);
-
-        //Analogico
-
-        if (Analogico)
-        {
-
-            v = Input.GetAxis("Vertical");
-            h = Input.GetAxis("Horizontal");
-        }
-
-        //Digital
-        else
-        {
-
-            if (moverCima)
-            {
-                v = 1;
-            }
-            else if (moverBaixo)
-            {
-                v = -1;
-            }
-            else
-            {
-                v = 0;
-            }
-
-            if (moverDireita)
-            {
-                h = 1;
-            }
-            else if (moverEsquerda)
-            {
-                h = -1;
-            }
-            else
-            {
-                h = 0;
-            }
-        }
-        this.rb.velocity = new Vector2(h * Velocity, v * Velocity);
-
-
-
-        //Identifica SpaceBar
-
-     
-    
-
-
-
-
-
-
+        float h;        
+        h = Input.GetAxis("Horizontal");
+        currentRotation -= rotationSensitivity * Time.deltaTime * h;
     }
 
     public void OnTriggerEnter2D(Collider2D collider)
